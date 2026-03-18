110c7f62d0a8635d8a0adfc701690b9fed041878
Łukasz Kułacz <lukasz.kulacz@rimedolabs.com>
2026-03-18 15:08:10 +0100

    Nowy test wydajności
    Nowy test wydajności dla metody sprawdzającej poprawność przypisania najemców do mieszkań

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,13 +1,18 @@
 import time
 import pytest
 
-from src.models import Apartment, Parameters
+from src.models import Apartment, Tenant, Parameters
 from src.manager import Manager
 
 
 def _create_n_apartments(n: int) -> dict:
     """Return a dict of n apartments with keys 'apart-0', 'apart-1', ..., 'apart-(n-1)'."""
     return {f"apart-{i}": Apartment(key=f"apart-{i}", name=f"Apart {i}", area_m2=100.0, location=f"{i} Main St", rooms={}) for i in range(n)}
+
+def _create_n_tenants(n: int, m: int) -> dict:
+    """Return a dict of n tenants with keys 'tenant-0', 'tenant-1', ..., 'tenant-(n-1)'. Every m-th tenant is assigned to an existing apartment, the rest are assigned to non-existing apartments."""
+    return {f"tenant-{i}": Tenant(key=f"tenant-{i}", name=f"Tenant {i}", apartment=f"apart-{i % m}", room='room-example',
+                                  deposit_pln=0.0, rent_pln=0.0, date_agreement_from='2025-01-01', date_agreement_to='2025-12-31') for i in range(n)}
 
 
 def test_search_for_apartment_large_dataset():
@@ -38,3 +43,28 @@ def test_search_for_apartment_large_dataset():
         f"Searching for non-existing apartment in {N} apartments took {fail_search_time:.3f}ms, limit {ALLOWED_SEARCH_TIME_MS}ms"
     )
 
+def test_check_tenants_apartment_keys_large_dataset():
+    """Checking tenants' apartment keys in a dataset of 10 000 apartments and 100 000 tenants should complete within the time limit."""
+    ALLOWED_CREATE_TIME_S = 10  # 10 seconds   
+    ALLOWED_CHECK_TIME_MS = 10  # 10 milliseconds
+    N_APARTMENTS = 10_000
+    N_TENANTS = 1_000_000
+
+    manager = Manager(Parameters()) 
+
+    create_time = time.perf_counter()
+    manager.apartments = _create_n_apartments(N_APARTMENTS)
+    manager.tenants = _create_n_tenants(N_TENANTS, N_APARTMENTS + 1)  # every 10001-th tenant is assigned to a non-existing apartment  
+    create_time = (time.perf_counter() - create_time)  # convert to seconds
+
+    check_time = time.perf_counter()
+    result = manager.check_tenants_apartment_keys()
+    check_time = (time.perf_counter() - check_time) * 1e3  # convert to milliseconds
+
+    assert result is False
+    assert check_time < ALLOWED_CHECK_TIME_MS, (
+        f"Checking tenants' apartment keys in {N_APARTMENTS} apartments and {N_TENANTS} tenants took {check_time:.3f}ms, limit {ALLOWED_CHECK_TIME_MS}ms"
+    )
+    assert create_time < ALLOWED_CREATE_TIME_S, (
+        f"Creating {N_APARTMENTS} apartments and {N_TENANTS} tenants took {create_time:.3f}s, limit {ALLOWED_CREATE_TIME_S}s"
+    )
